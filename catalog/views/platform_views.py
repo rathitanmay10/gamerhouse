e450b76d4d2490d6f4eaf8eb6195db60a8cd62ef
tanmayrathi-gkmit <tanmayrathi@gkmit.co>
2026-01-27 21:14:39 +0530

    feat(catalog): add search, filtering, and response caching

--- catalog/views/platform_views.py
+++ catalog/views/platform_views.py
@@ -1,7 +1,9 @@
+from rest_framework import filters
 from rest_framework.viewsets import ModelViewSet
 
 from catalog.models import Platform
 from catalog.serializers import PlatformSerializer
+from core.decorators import drf_cache_response, drf_invalidate_cache
 from core.permissions import IsSuperAdminOrReadOnly
 
 
@@ -17,3 +19,25 @@ class PlatformViewSet(ModelViewSet):
     serializer_class = PlatformSerializer
     permission_classes = [IsSuperAdminOrReadOnly]
     http_method_names = ["get", "post", "patch", "delete", "head", "options"]
+    filter_backends = [filters.SearchFilter]
+    search_fields = ["name"]
+
+    @drf_cache_response(prefix="platforms:list")
+    def list(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
+
+    @drf_cache_response(prefix="platforms:detail")
+    def retrieve(self, request, *args, **kwargs):
+        return super().retrieve(request, *args, **kwargs)
+
+    @drf_invalidate_cache(pattern="cache:platforms*")
+    def perform_create(self, serializer):
+        return super().perform_create(serializer)
+
+    @drf_invalidate_cache(pattern="cache:platforms*")
+    def perform_update(self, serializer):
+        return super().perform_update(serializer)
+
+    @drf_invalidate_cache(pattern="cache:platforms*")
+    def perform_destroy(self, instance):
+        return super().perform_destroy(instance)
